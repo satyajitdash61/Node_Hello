a17836acb93fa4e693c5b59cb419086f5ab57cc2
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-11 19:04:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = process.env.PORT ;
+const port = process.env.PORT || 4000 ;
 const server = http.createServer((req, res) => {
  res.statusCode = 200;
  res.end('Hello Node!\n');
