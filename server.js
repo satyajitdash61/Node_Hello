03396f0e59075d614d9d15bca0f7dff432256627
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-11 19:01:27 +0530

    updating

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-var http = require("http");
-
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+const http = require('http');
+const port = process.env.PORT ;
+const server = http.createServer((req, res) => {
+ res.statusCode = 200;
+ res.end('Hello Node!\n');
+});
+server.listen(port, () => {
+ console.log(`Server running on http://localhost:${port}/`);
+});
